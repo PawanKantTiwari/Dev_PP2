ee2c84f2e9c08149325d464eaa40794abe71b980
Pawan <p.k.t.7042@gmail.com>
2021-03-07 19:50:44 +0530

    1st commit in this Activity

---
+++ Lec3_cat_Command/wCats.js
@@ -0,0 +1,32 @@
+//wCats stands for Windows Cat ,, actually cat command is used in Linux 
+//But here we are seenig that how to use cat command using "fs" in javascript in windows 
+
+let content = process.argv.slice(2);
+let fs = require("fs");
+// console.log(content);
+
+let flags = [];
+let files = [];
+
+for(let i=0 ; i<content.length ; i++){
+    // "-s"
+    if( content[i].startsWith('-') ){
+        flags.push(content[i])
+    }
+    else{
+        files.push(content[i]);
+    }
+}
+
+// console.log(flags);
+// console.log(files);
+// flags X
+
+// for files output
+let fileKaData = "";
+for(let i=0 ; i<files.length ; i++){
+    // f1.txt => f2.txt
+    fileKaData += fs.readFileSync( files[i] ) + "";
+}
+
+console.log(fileKaData);
