2f7062f9ebb0e661d98a4d581fd9ffac4de666c4
Pawan <p.k.t.7042@gmail.com>
2021-04-10 16:20:36 +0530

    first commit in hackathon

---
+++ hackathon/first.js
@@ -0,0 +1,47 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+
+let typedWords;
+
+//web scraping
+
+
+
+//Automation
+let browserOpenPromises = puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"]
+});
+
+browserOpenPromises.then(function(browser){
+    let allOpenedTabPromises =  browser.pages();
+    return allOpenedTabPromises;
+}).then(function(pages)
+{ 
+    firstOpenedTab = pages[0];
+    let pageOpenPromises = firstOpenedTab.goto("https://thepracticetest.com/typing/tests/practice-paragraphs/");
+    let url = "https://thepracticetest.com/typing/tests/practice-paragraphs/";
+        request(url,cb);
+        function cb(error,response,data)
+        {
+            parseData(data);
+        }
+
+        function parseData(html)
+        {
+        let ch = cheerio.load(html);
+        let waitPromise = firstOpenedTab.waitForSelector("#typed",{visible:true});
+        let words = ch('#typed');
+        typedWords = ch(words).text();
+        // fs.writeFileSync("./typedWords.html",words+"");
+        }
+    return pageOpenPromises;
+}).then(function(){
+    let url = "https://thepracticetest.com/typing/tests/practice-paragraphs/";
+    let idTypePromise = firstOpenedTab.type("#typing",typedWords);
+    return idTypePromise;
+})
