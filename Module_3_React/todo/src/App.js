e7788283a52e94d975d1be20f874d8004f4a0834
Pawan Kant Tiwari <p.k.t.7042@gmail.com>
2021-06-25 13:27:20 +0530

    todo 1st step

--- Module_3_React/todo/src/App.js
+++ Module_3_React/todo/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import InputBox from "./components/InputBox/InputBox.jsx";
-import TodosList from "./components/TodosList/TodosList.jsx";
+
+import InputBox from "./components/InputBox/InputBox"
+import TodoList from "./components/TodoList/TodoList";
 
 class App extends Component {
-  state = {};
+  state = {
+    todos : [
+      {id : "1" , todo : "HTML"},
+      {id : "2" , todo : "CSS"},
+      {id : "3" , todo : "CSS"},
+      {id : "4" , todo : "CSS"},
+      {id : "5" , todo : "CSS"},
+    ]
+  }
   render() {
-    return (
-      <div className="App">
-        <InputBox></InputBox>
-        <TodosList></TodosList>
-      </div>
-    );
+    let todos = this.state.todo;
+    return <div className="App">
+              <InputBox></InputBox>
+              <TodoList todos =  {this.state.todos} ></TodoList>
+           </div>;
   }
 }
 
